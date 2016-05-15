73749d8409387094e1177f231cf84c74ca52dc37
刘鹏 <lp_aideals@hotmail.com>
2016-05-15 16:27:40 +0800

    Initial Commit

---
+++ scrollImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollImage
+//
+//  Created by 鹏 刘 on 16/5/15.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollImage
+//
+//  Created by 鹏 刘 on 16/5/15.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
